9d5725798bffe8dfd701f472820e166cf092e400
Mohammed0704 <116527392+Mohammed0704@users.noreply.github.com>
2023-07-13 20:57:26 -0400

    status

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    const takeoff = document.getElementById("takeoff");
+
+    takeoff.addEventListener("click", function () {
+        console.log("I clicked the button!");
+    });
+});
+
+
+
+/*
 let takeoff = getElementById("takeoff");
+
 takeoff.addEventListener("click", function () {
     let windowConfirm = window.confirm("Confirm that the shuttle is ready for takeoff.");
     if (windowConfirm){
-        document.getElementById("flightStatus") = "Shuttle in flight"
+        document.getElementById("flightStatus") = "Shuttle in flight";
+
     }
 });
+
+*/
